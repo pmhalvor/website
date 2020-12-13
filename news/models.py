d28f320935252a0372e35bd245e3c1acff5a461f
pmhalvor <pmchalvorsen@gmail.com>
2020-12-13 02:37:52 +0100

    make post model in VSC (easier)

--- news/models.py
+++ news/models.py
@@ -7,6 +7,7 @@ import datetime
 class Post(models.Model):
     post_title = models.CharField('title', max_length=100)
     pub_date = models.DateTimeField('date published')
+    post_html = models.CharField('file', max_length=1000000)
     def __str__(self):
         return self.post_title
 
