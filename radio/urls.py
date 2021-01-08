83db575bbcdfb9ded180662fb29d71f94846da71
pmhalvor <pmchalvorsen@gmail.com>
2021-01-08 14:58:36 +0000

    receive results. work on suggest

--- radio/urls.py
+++ radio/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+from .worker import search
 
 urlpatterns = [
     path('', views.radio, name='radio'),
     path('current/', views.Http_current, name='current'),
     path('recents/', views.Http_recents, name='recents'),
+    path('search/', search.Http_search, name='search'),
 ]
