e2a52a4dc2b6026f80684c60dee0a2e93bbf2897
pmhalvor <pmchalvorsen@gmail.com>
2021-01-10 13:12:51 +0000

    start rewrite submit

--- radio/static/js/submit.js
+++ radio/static/js/submit.js
@@ -1,36 +1,33 @@
 $("#recommend").click(function(){
-    var submitted = [];
-    $(".recommended").each( function() {
-        var artists = [];
-        $(this).find(".person").each(function(){
-            artists.push($(this).text());
-        });
-        var track  = $(this).find("#track").text();
-        var id = this.id;
-        submitted.push({'artists':artists, 'id':id, 'track':track});
-    });
+    $(".recommended").each(insert(item));
     $("#recommendations").empty();
-    insert(submitted);
 });
 
-function insert(items){
-    if (items.length<1){
-        console.log("Empty recommendations! Try searching for and selecting a track.");
-    }
-    else{
-        console.log(csrftoken);
-        console.log(items);
-        const request = new Request(
-            "submit",
-            {headers: {'X-CSRFToken': csrftoken},
-            method: 'POST',
-            body: '{"items":'+items+'}'}
-        );
-        fetch(request).then(function(response) {
-            console.log(response.status==200);
-            console.debug(response);
-        });
-        
-    }
-    console.log("finito");
+function insert(item){
+    console.log(item);
+    console.log("push to db");
+    // return {'artists':item.artists, 'id':item.id, 'track':item.track};
 }
+
+
+// function insert_all(items){
+//     if (items.length<1){
+//         console.log("Empty recommendations! Try searching for and selecting a track.");
+//     }
+//     else{
+//         console.log(items);
+//         console.log(csrftoken);
+//         const request = new Request(
+//             "submit",
+//             {headers: {'X-CSRFToken': csrftoken},
+//             method: 'POST',
+//             body: '{"items":'+items+'}'}
+//         );
+//         fetch(request).then(function(response) {
+//             console.log(response.status==200);
+//             console.debug(response);
+//         });
+        
+//     }
+//     console.log("finito");
+// }
