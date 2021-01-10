9144dfd92fd0157d2ec5fdfc21c09339405e081e
pmhalvor <pmchalvorsen@gmail.com>
2021-01-10 14:51:03 +0000

    suggest model complete

--- radio/static/js/submit.js
+++ radio/static/js/submit.js
@@ -4,6 +4,7 @@ $("#recommend").click(function(){
 });
 
 function insert(){
+    console.log("inserting row...");
     var artists = get_artists($(this));
     var track = $(this).find("#track").html();
     var track_id = this.id;
@@ -12,11 +13,10 @@ function insert(){
         'track': track,
         'track_id': track_id,
     }
-    console.log(data);
+    console.log(artists, track, track_id);
 
     console.log("getting token...");
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-    console.log(csrftoken);
     
     console.log("pushing to db...");
     $.ajax({
@@ -24,11 +24,10 @@ function insert(){
         data: data,
         'X-CSRFToken': csrftoken,
         success:function(response){
-            console.debug(response);
+            console.log(response);
+            console.log("insert complete");
         }
     });
-
-    console.log("complete.");
 }
 
 function get_artists(entry){
@@ -37,20 +36,4 @@ function get_artists(entry){
         a.push($(this).html());
     });
     return a;
-}
-
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
 }
--- radio/worker/submit.py
+++ radio/worker/submit.py
@@ -1,7 +1,6 @@
 from radio.models import Suggest
 from tools.table import to_dt
-from django.http import HttpResponse, HttpRequest
-import json
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_protect
 
 @csrf_protect
@@ -9,26 +8,12 @@ def submit_suggestions(request):
     artists = request.GET.getlist('artists[]')
     track = request.GET.get('track')
     track_id = request.GET.get('track_id')
-    return HttpResponse(f'{artists}, {track}, {track_id}')
-    # return HttpResponse('made it to django and back again')
+    sub_date= to_dt()
 
-# if request.POST.get('item'):
-# else:
-#     return HttpResponse("did not work")
-# data = json.loads(str(request.GET))
-# # data = request.POST.get('item')
-# # artists = data.get('artists')
-# # artists = request.POST['artists']
-# # track = request.POST['track']
-# # track_id = request.GET['id']
-# # sub_date= to_dt()
-# # artists = request.POST['artists']
-# # track = request.POST['track']
-# # track_id = request.GET['id']
-# # sub_date= to_dt()
+    artists = str([artist for artist in artists]).strip("[]\"'") # comma seperated artists
+    
+    # add to db
+    new = Suggest(artists=artists, sub_date=sub_date, track=track, track_id=track_id)
+    new.save()
 
-# # new = Update(artist=artist, sub_date=sub_date, track=track, track_id=track_id)
-# # new.save()
-
-# return HttpResponse("{'msg': 'success', 'datatype':"+str(data)+"}", content_type="application/json")
-	
+    return HttpResponse(f'{track_id}: {artists}, {track}')
