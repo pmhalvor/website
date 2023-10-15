f317afd876dd9130542d7a56b06335e41fcceee9
pmhalvor <pmchalvorsen@gmail.com>
2023-10-15 14:48:53 +0200

    ensure envvars present on server + small fixes

--- phsite/wsgi.py
+++ phsite/wsgi.py
@@ -6,11 +6,25 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
-
+import json
 import os
 
 from django.core.wsgi import get_wsgi_application
 
+# load env vars from local json via absolute path
+with open("/home/pmhalvor/site/phsite/config.json") as f:
+    config = json.load(f)
+
+# check required keys are actually present in config.json
+for required_key in ['AZURE_STORAGE', 'DJANGO_SECRET', 'POSTGRES_PASSWORD', 'ROOT', 'SPOTIFY_CLIENT_ID', 'SPOTIFY_CLIENT_SECRET']:
+    assert required_key in config.keys()
+
+# force feed environment vars, fail if var not in config.json 
+for key, value in config.items():
+    os.environ.setdefault(key, value)
+
+# added in tutorial
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'phsite.settings')
+
 
 application = get_wsgi_application()
