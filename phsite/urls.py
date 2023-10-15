1d144f33fc357a03aa4f4ee0942b6e2e2ec80b5d
pmhalvor <pmchalvorsen@gmail.com>
2023-10-15 19:07:11 +0200

    comment regex change in urls path

--- phsite/urls.py
+++ phsite/urls.py
@@ -23,11 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('radio/', include('radio.urls')),
+    # use regex paths for flexible urls
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-    # these last two paths giving warning. solve this conflict for robustness
 ]
 
 
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
