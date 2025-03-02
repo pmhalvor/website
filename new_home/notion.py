42916597e86c36e1a4f1b677914b625b9bd58740
Per Halvorsen <pmchalvorsen@gmail.com>
2025-03-02 14:16:55 +0100

    add db results parsers for the pages of the site

--- new_home/notion.py
+++ new_home/notion.py
@@ -2,22 +2,7 @@ import time
 import json
 import os
 from notion_client import Client
-
-class NotionDatabase:
-    def __init__(self, token, database_id):
-        self.notion = Client(auth=token)
-        self.database_id = database_id
-        
-    def get_all_items(self):
-        response = self.notion.databases.query(database_id=self.database_id)
-        return response["results"]
-        
-    def get_item(self, page_id):
-        return self.notion.pages.retrieve(page_id=page_id)
-        
-    # Additional CRUD methods as needed
-
-
+from datetime import datetime
 
 class CachedNotionClient:
     def __init__(self, token, cache_dir='./notion_cache', cache_ttl=3600):
@@ -65,5 +50,123 @@ class CachedNotionClient:
             if last_cache:
                 return last_cache
             raise e
-            
-    # Add additional methods for other Notion API calls
+
+
+def parse_about_results(results):
+    """
+    Identifier: Name
+    Content: Text
+    """
+    parsed_results = []
+    for result in results:
+        parsed_result = {
+            'title': result['properties']['Name']['title'][0]['text']['content'],
+            'content': [line.get("plain_text") for line in result['properties']['Text']['rich_text']]
+        }
+        parsed_results.append(parsed_result)
+    return parsed_results
+
+
+def parse_cv_results(results):
+    """
+    Identifier: Name
+    Fields:
+        category
+        title
+        subtitle
+        location
+        start_date
+        end_date
+    """
+
+    parsed_results = []
+    for result in results:
+
+        parsed_result = {
+            'id': result['properties']['Name']['title'][0]['text']['content'],
+            'category': result['properties']['category']['select']['name'],
+            'title': result['properties']['title']['rich_text'][0]['text']['content'],
+            'subtitle': extract_optional_text(result, 'subtitle'),
+            'location': extract_optional_text(result, 'location'),
+            'start_date': extract_date(result, 'start'),
+            'end_date': extract_date(result, 'end')
+        }
+        parsed_results.append(parsed_result)
+    return parsed_results
+
+
+def parse_notes_results(results):
+    """
+    Parse content populating Notes and Updates sections
+
+    Identifier: Name
+    Fields:
+        title
+        description
+        publish_date
+        file
+        image
+        tags
+    """
+    parsed_results = []
+
+    for result in results:
+        parsed_result = {
+            'id': result['properties']['Name']['title'][0]['text']['content'],
+            'title': result['properties']['title']['rich_text'][0]['text']['content'],
+            'description': result['properties']['description']['rich_text'][0]['text']['content'],
+            'publish_date': extract_date(result, 'start'),
+            'file': extract_url(result, 'file'),
+            'image': extract_url(result, 'image'),
+            'tags': [tag['name'] for tag in result['properties']['tags']['multi_select']]
+        }
+        parsed_results.append(parsed_result)
+    return parsed_results
+
+
+# utils 
+def pp(content):
+    print(json.dumps(content, indent=2))
+
+
+def extract_optional_text(result, key):
+    value = result['properties'][key].get("rich_text", [])
+    if len(value) > 0:
+        return value[0]['plain_text']
+    return ""
+
+
+def extract_date(result, key):
+    value = result['properties']['date']['date'].get(key)
+    if value is None:
+        return datetime.now().isoformat()
+    return datetime.strptime(value, "%Y-%m-%d").isoformat()
+
+
+def extract_url(result, key):
+    value = result['properties'][key].get("files", [])
+    if len(value) > 0:
+        if 'external' in value[0]:
+            return value[0]['external']['url']
+        return value[0]['file']['url']
+    return ""   
+
+
+if __name__ == "__main__":
+    from config import Env
+
+    env = Env("new_home/.env")
+
+    sdb_client = CachedNotionClient(env.notion_sitedb_token)
+
+    about_data = sdb_client.get_database(env.notion_sitedb_about_id)
+    cv_data = sdb_client.get_database(env.notion_sitedb_cv_id)
+    notes_data = sdb_client.get_database(env.notion_sitedb_notes_id)
+    updates_data = sdb_client.get_database(env.notion_sitedb_updates_id)
+
+    pp(parse_about_results(about_data['results']))
+    pp(parse_cv_results(cv_data['results']))
+    pp(parse_notes_results(notes_data['results']))
+    pp(parse_notes_results(updates_data['results']))
+    
+    print("Done.")
