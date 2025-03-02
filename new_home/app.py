bf8a0cde83fb4e190fce7c98ca380598685e74ef
Per Halvorsen <pmchalvorsen@gmail.com>
2025-03-02 14:32:39 +0100

    update flask app w/ old home structure

--- new_home/app.py
+++ new_home/app.py
@@ -1,19 +1,55 @@
-from flask import Flask, render_template
-from notion import CachedNotionClient
+from flask import Flask, render_template, redirect, url_for
+from notion import CachedNotionClient, parse_about_results, parse_cv_results, parse_notes_results
 from config import Env
 from flask import request
 
-env = Env() # use when running python app.py
-# env = Env("new_home/.env") # use when running python new_home/app.py
+env = Env("new_home/.env") # use when running python new_home/app.py
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-NOTION_TOKEN = env.notion_token
+NOTION_TOKEN = env.notion_sitedb_token
 DATABASE_ID = env.notion_database_id
 CACHE_DIR = "./notion_cache"
 CACHE_TTL = 3600
 
 notion_client = CachedNotionClient(token=NOTION_TOKEN, cache_dir=CACHE_DIR, cache_ttl=CACHE_TTL)
+
+@app.route('/')
+def home():
+    updates_data = notion_client.get_database(env.notion_sitedb_update_id)
+    updates = parse_notes_results(updates_data['results'])
+    return render_template('index.html', updates=updates[:7]) # most recent 7
+
+@app.route('/updates')
+def updates():
+    updates_data = notion_client.get_database(env.notion_sitedb_update_id)
+    updates = parse_notes_results(updates_data['results'])
+    return render_template('updates.html', updates=updates)
+
+@app.route('/about')
+def about():
+    about_data = notion_client.get_database(env.notion_sitedb_about_id)
+    about = parse_about_results(about_data['results'])
+    return render_template('about.html', about=about)
+
+@app.route('/cv')
+def cv():
+    cv_data = notion_client.get_database(env.notion_sitedb_cv_id)
+    cv = parse_cv_results(cv_data['results'])
+    # Separate into categories like in Django views
+    work = [item for item in cv if item['category'] == 'Work']
+    education = [item for item in cv if item['category'] == 'Education']
+    languages = [item for item in cv if item['category'] == 'Language']
+
+    return render_template("cv.html", cv_list=cv, work_list=work, edu_list=education, lang_list=languages)
+
+
+@app.route('/notes')
+def notes():
+    notes_data = notion_client.get_database(env.notion_sitedb_notes_id)
+    notes = parse_notes_results(notes_data['results'])
+    return render_template("notes.html", notes=notes)
+
 
 @app.route('/rir')
 def rir():
@@ -31,7 +67,8 @@ def rir():
     end = start + per_page
     paginated_items = items[start:end]
 
-    return render_template("index.html", items=paginated_items, page=page)
+    return render_template("rir.html", items=paginated_items, page=page)
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)  
+    app.run(debug=True, port=5001)
--- new_home/config.py
+++ new_home/config.py
@@ -1,21 +1,26 @@
-from pydantic import Field
-from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
+import os
 
-class Env(BaseSettings):
-    notion_database_id: str = Field(..., env='NOTION_DATABASE_ID', title='Notion Database ID')
-    notion_token: str = Field(..., env='NOTION_TOKEN', title='Notion Token')
-    root: str = Field(..., env='ROOT', title='Root path')
-    spotify_client_id: str = Field(..., env='SPOTIFY_CLIENT_ID', title='Spotify Client ID')
-    spotify_client_secret: str = Field(..., env='SPOTIFY_CLIENT_SECRET', title='Spotify Client Secret')
-
-    def __init__(self, env_file: str = ".env", **kwargs):
+class Env:
+    def __init__(self, env_file: str = ".env"):
         load_dotenv(env_file)
-        super().__init__(**kwargs)
+        self.env_vars = {key.lower(): value for key, value in os.environ.items()}
+
+    def __getattr__(self, item):
+        try:
+            return self.env_vars[item]
+        except KeyError:
+            raise AttributeError(f"'Env' object has no attribute '{item}'")
+
 
 if __name__ == "__main__":
-    env = Env()
+    env = Env("new_home/.env")
+    N = 5
     
-    print(f"Notion token:       {env.notion_token[:5]}...")
-    print(f"Notion database id: {env.notion_database_id[:5]}...")
-    print(f"Spotify client id:  {env.spotify_client_id[:5]}...")
+    print(f"Notion token:        {env.notion_token[:N]}...")
+    print(f"Notion database id:  {env.notion_database_id[:N]}...")
+    print(f"Notion SiteDB token: {env.notion_sitedb_token[:N]}...")
+    print(f"Notion SiteDB about: {env.notion_sitedb_about_id[:N]}...")
+    print(f"Notion SiteDB cv:    {env.notion_sitedb_cv_id[:N]}...")
+
+    print(f"Spotify client id:   {env.spotify_client_id[:N]}...")
