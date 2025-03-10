9b8ccb32d38ce45adc7de3f98d2932035b3fa4e9
Per Halvorsen <pmchalvorsen@gmail.com>
2025-03-10 23:27:00 +0100

    skip test requirring credential

--- tests/test_notion.py
+++ tests/test_notion.py
@@ -1,9 +1,11 @@
 from home.notion import CachedNotionClient
 from home.config import Env
+import pytest
 
 env = Env(".env")
 
 
+@pytest.mark.skip(reason="Notion API key required")
 def test_get_database():
     notion_client = CachedNotionClient(token=env.notion_sitedb_token, cache_dir="./notion_cache", cache_ttl=3600)
     database_data = notion_client.get_database(env.notion_sitedb_update_id)
