2505d6d6687c5761f3798cebb49a8aac8ef309fd
Per Halvorsen <per.halvorsen@reachsubsea.com>
2026-02-20 16:47:43 +0000

    add favicon

--- home/invite.py
+++ home/invite.py
@@ -5,12 +5,6 @@ def check_wedding_invite(args: dict, env: Env):
     """
     Check if the query parameters match the expected values for the wedding invite.
 
-    Expected query parameters:
-        - who
-        - when
-        - where
-        - activity
-
     Returns True if all parameters match, False otherwise.
     """
 
@@ -18,19 +12,6 @@ def check_wedding_invite(args: dict, env: Env):
     expected_when = env.wedding_invite_when
     expected_where = env.wedding_invite_where
     expected_activity = env.wedding_invite_activity
-
-    # print(
-    #     f"Expected: who={expected_who}, "
-    #     f"when={expected_when}, "
-    #     f"where={expected_where}, "
-    #     f"activity={expected_activity}"
-    # )
-    # print(
-    #     f"Received: who={args.get('who')}, "
-    #     f"when={args.get('when')}, "
-    #     f"where={args.get('where')}, "
-    #     f"activity={args.get('activity')}"
-    # )
 
     return (
         args.get("who") == expected_who and 
